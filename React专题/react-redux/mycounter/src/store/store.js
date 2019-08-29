d6e09dcdb9a47479cde7f2cfd176f685dc44ec1e
pubdreamcc <493310907@qq.com>
2019-08-30 01:09:04 +0800

    install redux-devtools (redux调试工具)

--- React专题/react-redux/mycounter/src/store/store.js
+++ React专题/react-redux/mycounter/src/store/store.js
@@ -1,4 +1,5 @@
 // redux 核心 store 对象模块
 import { createStore } from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
 import {increase} from './reducers'
-export default createStore(increase)
+export default createStore(increase, composeWithDevTools())
